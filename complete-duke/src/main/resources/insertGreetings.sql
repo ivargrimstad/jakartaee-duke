8828ed207b52a990be724305c20737e682c00529
Ivar Grimstad <ivar.grimstad@eclipse-foundation.org>
2020-11-09 16:08:57 +0100

    Update sample
    Signed-off-by: Ivar Grimstad <ivar.grimstad@eclipse-foundation.org>

--- complete-duke/src/main/resources/insertGreetings.sql
+++ complete-duke/src/main/resources/insertGreetings.sql
@@ -1,3 +1,3 @@
 -- noinspection SqlNoDataSourceInspectionForFile
 -- noinspection SqlDialectInspectionForFile
-insert into greeting(id,message,email) values (100, 'Howdy!','duke@dukes.java')
+insert into greeting(id,message,email) values (100, 'Howdy Jakarta EE 9!','duke@dukes.java')
