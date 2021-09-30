2df76a9df477c43bca981bf2e3b0c53419c57bd7
Ivar Grimstad <ivar.grimstad@eclipse-foundation.org>
2021-09-30 09:54:11 +0200

    Start demo

--- complete-duke/src/main/java/eu/agilejava/dukes/greeting/DukesService.java
+++ complete-duke/src/main/java/eu/agilejava/dukes/greeting/DukesService.java
@@ -1,8 +1,8 @@
 package eu.agilejava.dukes.greeting;
 
 
-import jakarta.ejb.Stateless;
-import jakarta.inject.Inject;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
 
 @Stateless
 public class DukesService {
