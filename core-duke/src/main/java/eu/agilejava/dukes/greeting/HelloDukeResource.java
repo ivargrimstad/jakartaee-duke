2b2ea63d500b0afb02ce1ea1e817720e3f6b38be
Ivar Grimstad <ivar.grimstad@eclipse-foundation.org>
2022-05-03 08:14:51 +0200

    Add starting point for core demo

---
+++ core-duke/src/main/java/eu/agilejava/dukes/greeting/HelloDukeResource.java
@@ -0,0 +1,25 @@
+package eu.agilejava.dukes.greeting;
+
+
+import eu.agilejava.dukes.annotation.Dukes;
+import javax.ejb.EJB;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+@Dukes(message = "Hi there!")
+@Path("hello")
+public class HelloDukeResource {
+
+    @EJB
+    private DukesService dukesService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public DukesGreeting sayHello() {
+
+        return dukesService.findGreeting();
+    }
+}
---
+++ core-duke/src/main/resources/insertGreetings.sql
@@ -0,0 +1,1 @@
+insert into greeting(id,message,email) values (100, 'Howdy Jakarta EE 8!','duke@dukes.java')
