da00909d1d2f7414e3195a35c3ad9e0b9b7f5427
Ivar Grimstad <ivar.grimstad@eclipse-foundation.org>
2022-05-26 11:30:26 +0200

    Add Core demo

--- core-duke/src/main/java/eu/agilejava/dukes/annotation/DukesExtension.java
+++ core-duke/src/main/java/eu/agilejava/dukes/annotation/DukesExtension.java
@@ -1,7 +1,8 @@
 package eu.agilejava.dukes.annotation;
 
 
-import jakarta.enterprise.inject.build.compatible.spi.BuildCompatibleExtension;
+import eu.agilejava.dukes.greeting.HelloDukeResource;
+import jakarta.enterprise.inject.build.compatible.spi.*;
 
 import java.util.logging.Logger;
 
@@ -9,7 +10,14 @@ public class DukesExtension implements BuildCompatibleExtension {
 
     private static final Logger LOGGER = Logger.getLogger("eu.agilejava.dukes");
 
+    @Enhancement(types = HelloDukeResource.class, withAnnotations = Dukes.class)
+    public void enhanceDuke(ClassConfig dukeAnnotated) {
 
+        LOGGER.info(() -> "==============================================================================");
+        LOGGER.info(() -> "Found @Dukes annotated class:" + dukeAnnotated.info().name());
+        LOGGER.info(() -> "Dukes message is: " + dukeAnnotated.info().annotation(Dukes.class).member("message").asString());
+        LOGGER.info(() -> "==============================================================================");
+    }
 }
 
 
--- core-duke/src/main/resources/insertGreetings.sql
+++
@@ -1,1 +0,0 @@
-insert into greeting(id,message,email) values (100, 'Howdy Jakarta EE 8!','duke@dukes.java')
