c6b6a76e7301f7fbcb2a1c1423a1114705a626d0
Ivar Grimstad <ivar.grimstad@eclipse-foundation.org>
2022-07-19 08:55:00 +0200

    Add small demo for Payara Cloud

---
+++ sushi-duke/src/main/java/eu/agilejava/dukes/CompleteDukeApplication.java
@@ -0,0 +1,10 @@
+package eu.agilejava.dukes;
+
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("")
+public class CompleteDukeApplication extends Application {
+
+}
---
+++ sushi-duke/src/main/java/eu/agilejava/dukes/greeting/HelloDukeResource.java
@@ -0,0 +1,17 @@
+package eu.agilejava.dukes.greeting;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("hello")
+public class HelloDukeResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sayHello() {
+        return "Duke's in the mood for sushi!";
+    }
+}
